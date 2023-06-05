ae4023e951e8be65c1b2abc2bb1ce0d502883969
Kyle Lehman <kyle.h.lehman@gmail.com>
2023-06-04 21:06:21 -0400

    Sets up release publishing via github-release-gradle-plugin
    https://github.com/BreadMoirai/github-release-gradle-plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,15 @@
 plugins {
+    alias(libs.plugins.githubRelease)
     alias(libs.plugins.reckon)
 }
 
+githubRelease {
+    token { System.getenv("GITHUB_TOKEN") }
+    owner { "kyhule" }
+    releaseName { version.toString() }
+    tagName { version.toString() }
+    generateReleaseNotes { true }
+}
 
 reckon {
     setDefaultInferredScope("minor")
